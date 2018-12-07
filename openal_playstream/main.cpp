7ab56b8bd961cdf64d1582453ba117217147d1ef
椎原 真浩 <winmain.cpp@gmail.com>
2018-12-07 11:52:16 +0900

    OpenALを開く処理と閉じる処理を追加

--- openal_playstream/main.cpp
+++ openal_playstream/main.cpp
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <al.h>
+#include <alc.h>
 
 int main(void) {
+    // OpenAL‚ðŠJ‚­
+    ALCdevice* device = alcOpenDevice(nullptr);
+    ALCcontext* context = nullptr;
+    if (device) {
+        context = alcCreateContext(device, nullptr);
+        alcMakeContextCurrent(context);
+    }
+
+    // OpenAL‚ð•Â‚¶‚é
+    alcMakeContextCurrent(nullptr);
+    alcDestroyContext(context);
+    alcCloseDevice(device);
     return 0;
 }
