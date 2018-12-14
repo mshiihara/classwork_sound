0b1174a173c0e20157d4019ffe68fbacdabeaba1
椎原 真浩 <winmain.cpp@gmail.com>
2018-12-14 10:18:01 +0900

    waveのフォーマット情報を取得しデバッグ表示

--- openal_playstream/main.cpp
+++ openal_playstream/main.cpp
@@ -2,6 +2,7 @@
 #include <al.h>
 #include <alc.h>
 #include <string.h>
+#include <guiddef.h>
 
 // ストリームに使用するバッファの数
 #define NUMBUFFERS 4
@@ -14,9 +15,22 @@ struct RIFFHeader {
     char          format[4];
 };
 
-struct RIFFChunk{
+struct RIFFChunk {
     char          tag[4];
     unsigned long size;
+};
+
+struct WAVEFMT {
+	unsigned short	usFormatTag;
+	unsigned short	usChannels;
+	unsigned long	ulSamplesPerSec;
+	unsigned long	ulAvgBytesPerSec;
+	unsigned short	usBlockAlign;
+	unsigned short	usBitsPerSample;
+	unsigned short	usSize;
+	unsigned short  usReserved;
+	unsigned long	ulChannelMask;
+    GUID            guidSubFormat;
 };
 
 int main(void) {
@@ -41,6 +55,7 @@ int main(void) {
 
     RIFFHeader riffHeader;
     RIFFChunk  riffChunk;
+    WAVEFMT waveFmt;
     // ファイルを開くのに成功
     if (fp) {
         // ヘッダを読み取り
@@ -51,7 +66,23 @@ int main(void) {
             while (fread(&riffChunk, 1, sizeof(RIFFChunk), fp) == sizeof(RIFFChunk)) {
                 // 読み取ったチャンクがfmt であるか確認
                 if (_strnicmp(riffChunk.tag, "fmt ", 4) == 0) {
+                    printf("fmt チャンクを発見\n");
+                    if (riffChunk.size <= sizeof(WAVEFMT)) {
+                        //フォーマット情報を読み取り
+                        fread(&waveFmt, 1, riffChunk.size, fp);
+                        printf("usFormatTag:%d\nusChannels:%d\nulSamplesPerSec:%d\nulAvgBytesPerSec:%d\nusBlockAlign:%d\nusBitsPerSample:%d\nusSize:%d\nusReserved:%d\nulChannelMask:%d\nguidSubFormat:%d\n",
+                            waveFmt.usFormatTag,
+                            waveFmt.usChannels,
+                            waveFmt.ulSamplesPerSec,
+                            waveFmt.ulAvgBytesPerSec,
+                            waveFmt.usBlockAlign,
+                            waveFmt.usBitsPerSample,
+                            waveFmt.usSize,
+                            waveFmt.usReserved,
+                            waveFmt.ulChannelMask,
+                            waveFmt.guidSubFormat);
 
+                    }
                 }
                 else {
                     // 次のチャンクへ移動
