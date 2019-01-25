0c6b6165ec4e42216ca90f5f340f61a88489eb09
椎原 真浩 <winmain.cpp@gmail.com>
2019-01-25 11:57:57 +0900

    wavefileを少し実装

--- openal_playstream/WaveFile.h
+++ openal_playstream/WaveFile.h
@@ -1,5 +1,15 @@
 #pragma once
 
 class WaveFile {
+public:
+    FILE* fp;
+    WAVEFMT* waveFmt;
+    WAVEFILEINFO* waveInfo;
 
+    void open(const char* filename);
+    long read(void* pData, int bufferSize);
+private:
+    bool checkRIFFHeader();
+    void readHeader();
+    readFMT_(RIFFChunk& riffChunk);
 };
