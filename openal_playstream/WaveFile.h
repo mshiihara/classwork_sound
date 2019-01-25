46050b960b799ed64da7c97664d806d597bc1233
椎原 真浩 <winmain.cpp@gmail.com>
2019-01-25 12:01:56 +0900

    構造体定義をwavefile.hに移動

--- openal_playstream/WaveFile.h
+++ openal_playstream/WaveFile.h
@@ -1,5 +1,44 @@
 #pragma once
 
+#include <stdio.h>
+
+// RIFFチャンクを格納する為の構造体
+struct RIFFHeader {
+    char          tag[4];
+    unsigned long size;
+    char          format[4];
+};
+
+struct RIFFChunk {
+    char          tag[4];
+    unsigned long size;
+};
+
+struct WAVEFMT {
+	unsigned short	usFormatTag;
+	unsigned short	usChannels;
+	unsigned long	ulSamplesPerSec;
+	unsigned long	ulAvgBytesPerSec;
+	unsigned short	usBlockAlign;
+	unsigned short	usBitsPerSample;
+	unsigned short	usSize;
+	unsigned short  usReserved;
+	unsigned long	ulChannelMask;
+    GUID            guidSubFormat;
+};
+enum WAVEFILETYPE
+{
+	WF_EX  = 1,
+	WF_EXT = 2
+};
+
+struct WAVEFILEINFO {
+    WAVEFILETYPE         wfType; // PCMなのかEXTENSIBLEなのかを区別する為の情報
+    WAVEFORMATEXTENSIBLE wfEXT;  // フォーマット情報
+    unsigned long waveSize;      // waveデータの大きさ
+    unsigned long waveChunkPos;  // waveチャンクのファイルポインタ
+};
+
 class WaveFile {
 public:
     FILE* fp;
